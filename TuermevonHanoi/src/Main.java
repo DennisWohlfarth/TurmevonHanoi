3d0a4fe474472cfc3e529509f23248230b5bf6aa
Wohlfarth, Dennis <den.wohlfarth.17@lehre.mosbach.dhbw.de>
2018-06-28 09:13:29 +0200

    kommentiert

--- TuermevonHanoi/src/Main.java
+++ TuermevonHanoi/src/Main.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
 public class Main {
 
 
@@ -14,12 +12,15 @@ public class Main {
      */
 	//List<Integer> list = new ArrayList<Integer>();
 	public List<Integer> start_Turm, hilfs_Turm, ziel_Turm;	// Unsere 3 Türme deklarieren
-	public int turmhoehe;
-	public boolean beendet=false;
+	public int turmhoehe;									// Turmhöhe kann später vom Benutzer eingegeben werden
     private void bewege (String start, String hilfsturm, String ziel, int n)
     {
 	if (n == 1){
 		
+		//Wenn n gleich 1 ist wird eine Scheibe von einem Turm der in der variablen start steht, auf den Turm der in der Vaiablen ziel steht verschoben
+		//Bedeutet die Variable start kan auch nach dem n-ten Durchgang für den Hilfsturm stehen
+		//Wir ordnen die Variable start zu und schreiben die letzte Zahl des Turmes in eine Zwischenvaliable "zahl"
+		// Anschließend wird das oberste Element von dem jeweiligen Turm entfernt 
 		int zahl=0;
 		if (start == "Startturm")
 		{			
@@ -37,6 +38,8 @@ public class Main {
 			ziel_Turm.remove(ziel_Turm.size()-1);
 		}
 		
+		//Wir weisen die Zwischenvaiable "zahl" nun dem Turm mit der Variablen "ziel" zu und fügen sie sozsagen auf den jeweiligen Turm drauf
+		// Auch hier handelt es sich nach n-tem Durchgang nicht immer um den Zielturm
 		if(ziel=="Startturm")
 		{
 			start_Turm.add(zahl);
@@ -50,7 +53,7 @@ public class Main {
 			ziel_Turm.add(zahl);
 		}
 		
-		System.out.println("Lege die oberste Scheibe" + zahl + "von " + start + " auf " + ziel + ".");
+		System.out.println("Lege die oberste Scheibe (" + zahl + ") von " + start + " auf " + ziel + ".");
 		//Elemente verschieben 
 		
 		
@@ -58,29 +61,35 @@ public class Main {
 	    
 	
 	else {
-            bewege(start, ziel, hilfsturm, n-1);// um 1 verkleinerten Turm von start auf b verschieben
-            bewege(start, hilfsturm, ziel, 1);//verschiebe oberste Scheibe von start nach c
-            bewege(hilfsturm, start, ziel, n-1);// um 1 verkleinerten Turm von b auf c verschieben
+            bewege(start, ziel, hilfsturm, n-1);// um 1 verkleinerten Turm von start auf hilfsturm verschieben
+            bewege(start, hilfsturm, ziel, 1);//verschiebe oberste Scheibe von start nach ziel
+            bewege(hilfsturm, start, ziel, n-1);// um 1 verkleinerten Turm von hilfsturm auf ziel verschieben
 	}
     }
 
     public void start ()
     {
-    	//Turmhöhe einlesen
+    	//Turmhöhe durch Scanner einlesen
     	Scanner scanner = new Scanner(System.in);
-		System.out.print("Anzhal der Scheiben eingeben:");
+		System.out.print("Die Größte Scheibe ist 0 und je größer sie Zahl desto kleiner die Scheibe! \n\nAnzhal der Scheiben eingeben:");
 		turmhoehe = scanner.nextInt();
 		scanner.close();
 		
+		
+		// Die 3 Türme als ArrayList deklarieren
     	start_Turm = new ArrayList<Integer>();
     	hilfs_Turm = new ArrayList<Integer>();
     	ziel_Turm = new ArrayList<Integer>();
+    	
+    	// Den Startturm mit dem Werten von 0 bis Turmhöhe-1 versehen.
+    	// Bedeutet 0 ist die größte und Turmhöhe -1 die kleinste Scheibe und die Scheiben werden also von 0 bis Turmhöhe -1 immer größer
     	for (int i =0; i<turmhoehe; i++){
     		start_Turm.add(i);	
     	}
     	
 	  bewege("Startturm", "Hilfsturm", "Zielturm", turmhoehe);
-	  beendet = true;
+	  // Am Ende die Werte des Zielturms ausgeben
+	  // Hier muss nun die selbe Reihenfolge wie bei Startturm sein, ansonsten würde der Algorytmus nicht stimmen
 	  System.out.println(ziel_Turm.toString());
     }
     
--- TuermevonHanoi/src/MainTest.java
+++ TuermevonHanoi/src/MainTest.java
@@ -10,26 +10,26 @@ class MainTest {
 	@Before
 	void init() {
 		test = new Main();
-		test.start();
+		test.start();		// Turme von Hanoi durchlaufen lassen
 		
 	}
 	
-	@Test
+	@Test		// Test wird automatisch gestartet
 	
-	void EndReihenfolgevergleichen()
+	void EndReihenfolgevergleichen()		// Es wird getestet ob der Zielturm, den wir nach Programmablauf erhalten, richtig angeordnet wurde
     {
-			init();
-    		for(int i=test.turmhoehe-1;i>0; i--)
+			init(); // initialisierung ausführen und Programm starten
+    		for(int i=test.turmhoehe-1;i>0; i--)		// Schleife mit Turmhöhe - 1 durchlaufen, da unsere größte Scheibe schon 0 ist und die kleiste somit Turmhöhe -1
         	{    			
-        		assertEquals(test.ziel_Turm.size()-1, i);
-        		test.ziel_Turm.remove(test.ziel_Turm.size()-1);
+        		assertEquals(test.ziel_Turm.size()-1, i);	// Oberstes Zielturmelement muss gleich der Turmhöhe entsprechen, weil 0 die größte scheibe ist
+        		test.ziel_Turm.remove(test.ziel_Turm.size()-1); // Oberstes Zieltumelemet entfernen und durch die Schleife mit dem daruter weiter vergleichen
         		
         	}
     	
     	
     }
 	
-	@Test 
+	//@Test 
 	
 
 }
